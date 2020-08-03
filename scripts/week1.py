d2af121fbdb78799b937c981e73640e90dc88067
Oliviatonorth <huiyuzhang010521@gmail.com>
2020-08-03 17:23:29 -0400

    prints hello capstone project

---
+++ scripts/week1.py
@@ -0,0 +1,6 @@
+import markdown
+html = markdown.markdown("this notebook will be mainly used \
+for the capstone project")
+import pandas as pd
+import numpy as np
+print("Hello Captstone Project Course!")
