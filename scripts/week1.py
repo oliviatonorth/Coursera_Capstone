4edf582746bd2794046144d0310dfc34a00c2b00
oliviatonorth <69115800+oliviatonorth@users.noreply.github.com>
2020-08-03 17:40:03 -0400

    Delete week1.py

--- scripts/week1.py
+++
@@ -1,6 +0,0 @@
-import markdown
-html = markdown.markdown("this notebook will be mainly used \
-for the capstone project")
-import pandas as pd
-import numpy as np
-print("Hello Captstone Project Course!")
